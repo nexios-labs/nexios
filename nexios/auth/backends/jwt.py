694ad92b3ce138465cc0c99743df6c53a52ddc6b
techwithdunamix <techwithdunamix@gmail.com>
2025-08-16 19:53:39 +0100

    fix(auth): add Callable import to jwt backend

--- nexios/auth/backends/jwt.py
+++ nexios/auth/backends/jwt.py
@@ -2,7 +2,7 @@ try:
     import jwt
 except ImportError:
     jwt = None
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Callable
 
 from nexios.auth.base import AuthenticationBackend, UnauthenticatedUser
 from nexios.config import get_config
