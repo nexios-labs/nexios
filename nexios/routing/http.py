a62877edbc6c2a3843a287050a5c1efde4a3cc04
techwithdunamix <techwithdunamix@gmail.com>
2025-10-01 17:26:31 +0100

    feat(routing): add request_content_type parameter to router
    - Introduce new parameter 'request_content_type' with possible values:
      'application/json', 'application/x-www-form-urlencoded', 'multipart/form-data'
    - Default value set to 'application/json'
    - Enhances the router's flexibility in handling different content types

--- nexios/routing/http.py
+++ nexios/routing/http.py
@@ -1340,6 +1340,15 @@ class Router(BaseRouter):
             """
             ),
         ] = False,
+        request_content_type: Annotated[
+            Optional[Literal["application/json", "application/x-www-form-urlencoded", "multipart/form-data"]],
+            Doc(
+                """
+                Request content type.
+                Example: 'application/json'
+            """
+            ),
+        ] = "application/json",
         **kwargs: Annotated[
             Dict[str, Any],
             Doc(
